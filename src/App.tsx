ca8ffaa978c2196ec97141b8b1810ced4981df0a
Louis Young <me@louisyoung.co.uk>
2021-04-01 18:43:44 +0100

    Add baisc category and expense rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,42 @@
+import { useState } from "react";
+import type { Expense } from "./types/expense";
+
+const mockCategories = ["Leisure", "Bills", "Shopping"];
+const mockExpenses = [
+  { label: "Gym", amount: 30, category: "Leisure", id: 0 },
+  { label: "Tax", amount: 200, category: "Bills", id: 1 },
+];
+
 export const App = () => {
-  return <div>Finances</div>;
+  const [categories, setCategories] = useState<string[]>(mockCategories);
+  const [expenses, setExpenses] = useState<Expense[]>(mockExpenses);
+
+  return (
+    <section>
+      <h1>Finances</h1>
+
+      <h2>Categories:</h2>
+
+      <ul>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ul>
+
+      <h2>Expenses:</h2>
+
+      <ul>
+        {expenses.map(({ id, label, amount, category }) => (
+          <li key={id}>
+            Label: {label}
+            <br />
+            Amount: {amount}
+            <br />
+            Category: {category}
+            <br />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
---
+++ src/types/expense.ts
@@ -0,0 +1,6 @@
+export interface Expense {
+  label: string;
+  amount: number;
+  category: string;
+  id: number;
+}
